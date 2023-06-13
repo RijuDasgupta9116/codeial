59a2869921eaf40cfde874eb0bfbd25da7d22108
RijuDasgupta9116 <riju.dasgupta@gcettb.ac.in>
2023-06-13 17:22:15 +0530

    Setup Mongodb connection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const expressLayouts = require('express-ejs-layouts');
+const db = require("./config/mongoose");
+
 // const path = require("path");
 const port = 8000;
 
