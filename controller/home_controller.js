dc1b6649fc5325714fe31183ab778fbe5b10f9e2
RijuDasgupta9116 <riju.dasgupta@gcettb.ac.in>
2023-07-23 16:13:29 +0530

    adding comment and post in the db

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -3,7 +3,12 @@ const Post = require("../models/post");
 module.exports.home = async function(req, res){
     // console.log(req.cookies);
     try {
-        let posts = await Post.find({}).populate('user').exec();
+        let posts = await Post.find({}).populate('user').populate({
+            path:'comments',
+            populate:{
+                path: 'user'
+            }
+        }).exec();
         return res.render('home',{
             title:"Codeial | Home",
             posts: posts
--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/post');
-
+const Comment = require('../models/comment');
 module.exports.create = async (req,res) =>{
     try {
         let post = await Post.create({
@@ -18,3 +18,16 @@ module.exports.create = async (req,res) =>{
     
 }
 
+module.exports.destroy = async function(req,res){
+    try {
+        let post = await Post.findById(req.params.id);
+        // .id means converting the object id into string
+        if(post.user == req.user.id){
+            post.remove();
+            let comment = await Comment.deleteMany({post: req.params.id})
+        }
+    } catch (error) {
+        
+    }
+
+}
